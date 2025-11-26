7b42c60810bb71e0ff0ff2b5e22e86eb969e20af
luis <luotasss@gmail.com>
2025-11-26 15:51:59 -0300

    creating the function to get the devices and plotting it on a new frame for linux

--- src/swap_info.rs
+++ src/swap_info.rs
@@ -4,9 +4,9 @@ use thiserror::Error;
 #[cfg(target_os = "windows")]
 use std::io;
 
+use proc_mounts::SwapIter;
 #[cfg(target_os = "linux")]
 use procfs::{self, Current, Meminfo};
-
 
 #[derive(Debug, Clone)]
 pub struct ProcessSwapInfo {
@@ -15,9 +15,20 @@ pub struct ProcessSwapInfo {
     pub swap_size: f64,
 }
 
-#[derive(Debug, Clone, Default)]
+#[cfg(target_os = "linux")]
+#[derive(Debug, Clone)]
+pub struct InfoSwap {
+    pub name: String,
+    pub kind: String,
+    pub size_kb: u64,
+    pub used_kb: f64,
+    pub priority: isize,
+}
 
+#[derive(Debug, Clone, Default)]
 pub struct SwapUpdate {
+    #[cfg(target_os = "linux")]
+    pub swap_devices: Vec<InfoSwap>,
     pub total_swap: u64,
     pub used_swap: u64,
 }
@@ -27,7 +38,7 @@ pub enum SizeUnits {
     #[default]
     KB,
     MB,
-    GB
+    GB,
 }
 
 #[cfg(target_os = "linux")]
@@ -45,6 +56,23 @@ pub enum SwapDataError {
     #[error("I/O error accessing system information: {0}")]
     Io(#[from] io::Error),
 }
+
+#[cfg(target_os = "linux")]
+pub fn get_swap_devices(unit: SizeUnits) -> std::io::Result<Vec<InfoSwap>> {
+    let mut out = Vec::new();
+    for swap in SwapIter::new()? {
+        let s = swap?;
+        out.push(InfoSwap {
+            name: s.source.to_string_lossy().into_owned(),
+            kind: s.kind.to_string_lossy().into_owned(),
+            size_kb: s.size as u64,
+            used_kb: convert_swap(s.used as u64, unit.to_owned()),
+            priority: s.priority,
+        });
+    }
+    Ok(out)
+}
+
 #[cfg(target_os = "linux")]
 pub fn get_processes_using_swap(unit: SizeUnits) -> Result<Vec<ProcessSwapInfo>, SwapDataError> {
     let mut swap_processes = Vec::new();
@@ -52,31 +80,26 @@ pub fn get_processes_using_swap(unit: SizeUnits) -> Result<Vec<ProcessSwapInfo>,
     for process_result in procfs::process::all_processes()? {
         match process_result {
             Ok(process) => {
-            	let pid = process.pid;
-            	if let Ok(status) = process.status() {
-                         if let Some(swap_kb) = status.vmswap {
-                             if swap_kb > 0 {
-                                 let name = match process.stat() {
-                                     Ok(stat) => stat.comm,
-                                     Err(_) => {
-                                         "unknown".to_string()
-                                     }
-                                 };
-                                 let swap_size = convert_swap(swap_kb, unit.clone());
-                                 let info = ProcessSwapInfo {
-                                     pid: pid as u32,
-                                     name,
-                                     swap_size,
-                                 };
-                                 swap_processes.push(info);
-                             }
-                         }
-             			
-             		}
-            },
-            Err(_) => {
-                
+                let pid = process.pid;
+                if let Ok(status) = process.status() {
+                    if let Some(swap_kb) = status.vmswap {
+                        if swap_kb > 0 {
+                            let name = match process.stat() {
+                                Ok(stat) => stat.comm,
+                                Err(_) => "unknown".to_string(),
+                            };
+                            let swap_size = convert_swap(swap_kb, unit.clone());
+                            let info = ProcessSwapInfo {
+                                pid: pid as u32,
+                                name,
+                                swap_size,
+                            };
+                            swap_processes.push(info);
+                        }
+                    }
+                }
             }
+            Err(_) => {}
         }
     }
 
@@ -87,36 +110,38 @@ pub fn get_processes_using_swap(unit: SizeUnits) -> Result<Vec<ProcessSwapInfo>,
 pub fn get_processes_using_swap(unit: SizeUnits) -> Result<Vec<ProcessSwapInfo>, SwapDataError> {
     let mut profile_page_processes = Vec::new();
 
-        match tasklist::Tasklist::new() {
-            Ok(tasks) => {
-                for task in tasks{
-                    let meminfo = task.get_memory_info();
+    match tasklist::Tasklist::new() {
+        Ok(tasks) => {
+            for task in tasks {
+                let meminfo = task.get_memory_info();
 
-                    let info = ProcessSwapInfo{
-                        pid: task.pid,
-                        name: task.pname,
-                        swap_size: convert_swap(meminfo.get_pagefile_usage() as u64 /1024, unit.clone())
-                    };
-                    profile_page_processes.push(info);
-                }
-
+                let info = ProcessSwapInfo {
+                    pid: task.pid,
+                    name: task.pname,
+                    swap_size: convert_swap(
+                        meminfo.get_pagefile_usage() as u64 / 1024,
+                        unit.clone(),
+                    ),
+                };
+                profile_page_processes.push(info);
             }
-            Err(_) => {
-            }
-        
+        }
+        Err(_) => {}
     }
 
     Ok(profile_page_processes)
 }
 
 #[cfg(target_os = "linux")]
-pub fn get_chart_info() -> Result<SwapUpdate, SwapDataError> {
+pub fn get_chart_info(unit: SizeUnits) -> Result<SwapUpdate, SwapDataError> {
     let meminfo = Meminfo::current()?;
-    
+
     let total_swap_kb = meminfo.swap_total / 1024;
     let used_swap_kb = meminfo.swap_total.saturating_sub(meminfo.swap_free) / 1024;
+    let swap_devices = get_swap_devices(unit)?;
 
     Ok(SwapUpdate {
+        swap_devices: swap_devices,
         total_swap: total_swap_kb,
         used_swap: used_swap_kb,
     })
@@ -124,8 +149,8 @@ pub fn get_chart_info() -> Result<SwapUpdate, SwapDataError> {
 
 #[cfg(target_os = "windows")]
 pub fn get_chart_info() -> Result<SwapUpdate, SwapDataError> {
-    use winapi::um::sysinfoapi::{GlobalMemoryStatusEx, MEMORYSTATUSEX};
     use std::mem::MaybeUninit;
+    use winapi::um::sysinfoapi::{GlobalMemoryStatusEx, MEMORYSTATUSEX};
 
     unsafe {
         let mut mem_status = MaybeUninit::<MEMORYSTATUSEX>::zeroed();
@@ -171,7 +196,7 @@ pub fn aggregate_processes(processes: Vec<ProcessSwapInfo>) -> Vec<ProcessSwapIn
     let mut aggregated_processes: Vec<ProcessSwapInfo> = name_to_info
         .into_iter()
         .map(|(name, (swap_size, count))| ProcessSwapInfo {
-            pid: count,  
+            pid: count,
             name,
             swap_size,
         })
